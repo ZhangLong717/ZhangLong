24994f4ac400460131fd0f54ad97089d8f84cf8a
Hgc <Hgc@Hgc-PC>
2014-11-14 23:19:09 +0800

    hangyongchol

--- assets/mine/js/firstpage.js
+++ assets/mine/js/firstpage.js
@@ -25,14 +25,41 @@ $(document).ready(function(){
     $(imenu).show();
     var lis = $(imenu).find("li");
    // lis[0].bind(showpopup1);
-    $(lis[0]).bind("click",showpopup1);
+    $(lis[0]).bind("click",{menuorder:"0"},showpopup1);
+    $(lis[1]).bind("click",{menuorder:"1"},showpopup1);
+    $(lis[2]).bind("click",{btnorder:n},showpopup2);
+    //$(lis[0]).bind("click",showpopup1);
     return true;
-}
-    function showpopup1()
-    {
-        $(this).parent().parent().css("display","none");
-        //alert("showpopup1");
-        $('#myModal').modal('show');
-        $(this).unbind("click");
     }
+    function showpopup1(event)//
+    {
+        var data = event.data;
+        var menuid = data.menuorder;
+        $(this).parent().parent().css("display","none");
+        //firstpage_type0dialog1
+        $("#firstpage_type0dialog"+menuid).modal('show');
+        var parent = $(this).parent();
+        $(parent).children().each(function(i){
+            $(this).unbind("click");
+        });
+    }
+    function showpopup2(event)
+    {
+        var data = event.data;
+        var btnid = data.btnorder;
+        $(this).parent().parent().css("display","none");
+        $("#firstpage_type1dialog"+btnid).modal('show');
+        var parent = $(this).parent();
+        $(parent).children().each(function(i){
+            $(this).unbind("click");
+        });
+    }
+    
+    
+    $("#table_first_1_function1").click(function(){
+        $("#firstpage_dialog1").modal('show');
+    });
+    $("#table_first_1_function2").click(function(){
+        $("#firstpage_dialog3").modal('show');
+    });
 });
---
+++ assets/mine/js/firstpage_1.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+    $(".tingyong").click(function(){
+        var rowid = $(this).parent().data("rowid");
+        //alert(rowid);
+        var tds = $(this).parent().parent().find("td");
+        var text1 = $(tds[1]).html();
+        text1 = "("+ text1 +")";
+        var item1 = $("span.text1")[0];
+        $("#customtable_dialog_tingyong").find(item1).html(text1);
+        //$("#customtable_dialog_tingyong").modal('show');
+        $("#customtable_dialog_tingyong").modal('show');
+    });
+    $(".huifu").click(function(){
+        var rowid = $(this).parent().data("rowid");
+        $("#customtable_dialog_mquanxian").modal('show');
+    });
+    $(".sanchu").click(function(){
+        var rowid = $(this).parent().data("rowid");
+        $("#customtable_dialog_mquanxian").modal('show');
+    });
+    $(".customtable>table>tbody>tr").click(function(){
+       //alert("sss") ;
+       window.location = "firstpage_1_table.html"
+    });
+});
+
